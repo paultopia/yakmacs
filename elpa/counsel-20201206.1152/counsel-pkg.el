485faea43a5af13a0b5f3fef68e836ad9d4f2fb5
Paul Gowder <paul.gowder@gmail.com>
2020-12-08 07:41:48 -0600

    putting my dependencies under version control because emacs packages are too fragile to trust a repo without versioning

---
+++ elpa/counsel-20201206.1152/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20201206.1152" "Various completion functions using Ivy" '((emacs "24.5") (swiper "0.13.0")) :commit "ab36ccb32322f2b500f947bbe10f62194a9ef6c8" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
---
+++ elpa/swiper-20201002.1049/swiper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "swiper" "20201002.1049" "Isearch with an overview. Oh, man!" '((emacs "24.5") (ivy "0.13.0")) :commit "ab36ccb32322f2b500f947bbe10f62194a9ef6c8" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
