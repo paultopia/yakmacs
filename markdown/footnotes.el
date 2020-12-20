3289b6a2bc71ba155601d2f84beae67894cc7b5c
Paul Gowder <paul.gowder@gmail.com>
2020-12-20 17:25:10 -0600

    identify footnote locations

--- markdown/footnotes.el
+++ markdown/footnotes.el
@@ -1,10 +1,20 @@
-; works
+(defun end-of-fn ()
+  "Assumes point is at beginning of fn bracket, jumps to end and returns value of point. Depends on smartparens."
+  (sp-end-of-sexp)
+  (point)
+  )
+
+
+;works
 (defun find-next-inline-footnote ()
   "Return a pair of points for starting caret and ending bracket of next footnote in current buffer."
   (interactive)
   (let* (
 	 (fn (search-forward "^["))
 	 (start (1- fn))
+	 (end (end-of-fn))
+	 (results (list start end))
 	 )
-    (sp-end-of-sexp)
+    (print results)
+    results
     ))
