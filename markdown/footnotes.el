828848661348690261d791ea9548c88a7f06d932
Paul Gowder <paul.gowder@gmail.com>
2020-12-20 18:01:57 -0600

    can identify locations of all footnotes

--- markdown/footnotes.el
+++ markdown/footnotes.el
@@ -4,17 +4,44 @@
   (point)
   )
 
+(defun check-for-next-fn ()
+  (interactive)
+  (let ((fn (search-forward "^[" nil t)))
+    (if fn
+       fn
+       0
+	)))
 
-;works
 (defun find-next-inline-footnote ()
-  "Return a pair of points for starting caret and ending bracket of next footnote in current buffer."
+  "Return a pair of points for starting caret and ending bracket of next footnote in current buffer. If not found, return a list beginning with -1."
   (interactive)
   (let* (
-	 (fn (search-forward "^["))
+	 (fn (check-for-next-fn))
 	 (start (1- fn))
 	 (end (end-of-fn))
 	 (results (list start end))
 	 )
-    (print results)
+    ;; (print results)
     results
     ))
+
+(defun get-all-inline-footnotes ()
+  "gets locations of all footnotes in buffer"
+  (interactive)
+  (save-excursion
+    (beginning-of-buffer)
+    (let ((curnote)
+	  (results)
+	  (curpoint 0))
+      (while (>= curpoint 0)
+	(setq curnote (find-next-inline-footnote))
+	(setq curpoint (car curnote))
+	(if (>= curpoint 0)
+	    (setq results (cons curnote results))
+	    )
+	)
+      (print results)
+      results
+      ))
+  )
+
