d5651a16db9f0500e5ae0336eb13199981625eac
Paul Gowder <paul.gowder@gmail.com>
2021-01-16 17:30:22 -0600

    keybindings kinda work in org agenda?  kinda...

--- org/capture.el
+++ org/capture.el
@@ -6,15 +6,18 @@
 
 (setq org-agenda-files '("~/Dropbox/org/inbox.org"))
 
+(setq rolusa_tasks "~/OneDrive - Northwestern University/tasks/rolusa_tasks.org")
+(setq netlev_tasks "~/OneDrive - Northwestern University/tasks/netlev_tasks.org")
+
 (add-to-list 'org-capture-templates
     '("r" "Rule of Law in USA Task"  entry
-      (file "~/repos/rol_in_usa/task_inbox.org")
+      (file rolusa_tasks)
 	"* TODO %?" :empty-lines 1))
 
-(add-to-list 'org-agenda-files "~/repos/rol_in_usa/task_inbox.org" 'append)
+(add-to-list 'org-agenda-files rolusa_tasks 'append)
 
 (add-to-list 'org-capture-templates
     '("n" "Networked Leviathan Task"  entry
-      (file "~/repos/networked-leviathan/task_inbox.org")
+      (file netlev_tasks)
 	"* TODO %?" :empty-lines 1))
-(add-to-list 'org-agenda-files "~/repos/networked-leviathan/task_inbox.org" 'append)
+(add-to-list 'org-agenda-files netlev_tasks 'append)
