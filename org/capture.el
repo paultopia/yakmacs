a1d895fac859da4630d8a730734bdaf69f7058b3
Paul Gowder <paul.gowder@gmail.com>
2021-01-15 22:59:18 -0600

    rudimentary agenda, but needs filenaming work

--- org/capture.el
+++ org/capture.el
@@ -4,12 +4,17 @@
     (file org-default-notes-file)
       "* TODO %?" :empty-lines 1)))
 
-(add-to-list 'org-capture-templates
-  '("r" "Rule of Law in USA Task"  entry
-    (file "~/repos/rol_in_usa/task_inbox.org")
-      "* TODO %?" :empty-lines 1))
+(setq org-agenda-files '("~/Dropbox/org/inbox.org"))
 
 (add-to-list 'org-capture-templates
-  '("n" "Rule of Law in USA Task"  entry
-    (file "~/repos/networked-leviathan/task_inbox.org")
-      "* TODO %?" :empty-lines 1))
+    '("r" "Rule of Law in USA Task"  entry
+      (file "~/repos/rol_in_usa/task_inbox.org")
+	"* TODO %?" :empty-lines 1))
+
+(add-to-list 'org-agenda-files "~/repos/rol_in_usa/task_inbox.org" 'append)
+
+(add-to-list 'org-capture-templates
+    '("n" "Networked Leviathan Task"  entry
+      (file "~/repos/networked-leviathan/task_inbox.org")
+	"* TODO %?" :empty-lines 1))
+(add-to-list 'org-agenda-files "~/repos/networked-leviathan/task_inbox.org" 'append)
