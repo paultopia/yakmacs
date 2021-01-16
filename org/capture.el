1d4554a38038e81b3373b3c5d3da0ca459c4b8a3
Paul Gowder <paul.gowder@gmail.com>
2021-01-15 22:37:19 -0600

    setup org capture

--- org/capture.el
+++ org/capture.el
@@ -1,15 +1,15 @@
 (setq org-default-notes-file "~/Dropbox/org/inbox.org")
-;; (setq org-capture-templates 
-;;   '("t" "Unfiled Task"  entry
-;;     (file org-default-notes-file)
-;;       "* TODO %?" :empty-lines 1))
+(setq org-capture-templates 
+  '(("t" "Unfiled Task"  entry
+    (file org-default-notes-file)
+      "* TODO %?" :empty-lines 1)))
 
-;; (add-to-list 'org-capture-templates
-;;   '("r" "Rule of Law in USA Task"  entry
-;;     (file "~/repos/rol_in_usa/task_inbox.org")
-;;       "* TODO %?" :empty-lines 1))
+(add-to-list 'org-capture-templates
+  '("r" "Rule of Law in USA Task"  entry
+    (file "~/repos/rol_in_usa/task_inbox.org")
+      "* TODO %?" :empty-lines 1))
 
-;; (add-to-list 'org-capture-templates
-;;   '("n" "Rule of Law in USA Task"  entry
-;;     (file "~/repos/networked-leviathan/task_inbox.org")
-;;       "* TODO %?" :empty-lines 1))
+(add-to-list 'org-capture-templates
+  '("n" "Rule of Law in USA Task"  entry
+    (file "~/repos/networked-leviathan/task_inbox.org")
+      "* TODO %?" :empty-lines 1))
