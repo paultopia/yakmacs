ec93d9edf1e283f89628c830e05ab829db82ca06
Paul Gowder <paul.gowder@gmail.com>
2021-01-30 19:56:45 -0600

    add keybindings for json

--- javascript/javascript.el
+++ javascript/javascript.el
@@ -1,19 +1,15 @@
-(straight-use-package 'js2-mode)
-(require 'js2-mode)
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+;; (straight-use-package 'js2-mode)
+;; (require 'js2-mode)
+;; (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
-(straight-use-package 'js2-refactor)
-(straight-use-package 'xref-js2)
-(require 'js2-refactor)
-(require 'xref-js2)
+;; (straight-use-package 'js2-refactor)
+;; (straight-use-package 'xref-js2)
+;; (require 'js2-refactor)
+;; (require 'xref-js2)
 
-(add-hook 'js2-mode-hook #'js2-refactor-mode)
+;; (add-hook 'js2-mode-hook #'js2-refactor-mode)
 
-(straight-use-package 'web-mode)
-
-(straight-use-package 'indium)
-(setq indium-chrome-executable "chrome")
-(require 'indium)
+;; (straight-use-package 'web-mode)
 
 (straight-use-package 'vue-mode)
 (add-to-list 'auto-mode-alist '("\\.vue\\'" . vue-mode))
